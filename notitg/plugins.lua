048acd3d0e7e36f47a7ee758e86f9969a0c0ca39
Sudospective <avery@sudospective.net>
2021-04-26 17:39:47 -0500

    Removed `loadscript` in favor of NotITG's updated `loadfile`

--- notitg/plugins.lua
+++ notitg/plugins.lua
@@ -1,6 +1,8 @@
 local path_to_plugins = GAMESTATE:GetCurrentSong():GetSongDir() .. 'plugins/'
 local af = Def.ActorFrame {}
-for _, v in ipairs({GAMESTATE:GetFileStructure(path_to_plugins)}) do
-    xero.loadscript('plugins/'..v)
+for _, filename in ipairs({GAMESTATE:GetFileStructure(path_to_plugins)}) do
+    if string.sub(filename, -4, -1) == '.lua' then
+        af[#af + 1] = assert(loadfile('plugins/'..filename))()
+    end
 end
 return af
--- template/default.lua
+++ template/default.lua
@@ -1,51 +1,14 @@
 _G.xero = {}
-
-function xero.loadscript(path)
-	-- load the song directory
-	local songdir = GAMESTATE:GetCurrentSong():GetSongDir()
-	-- attempt to load lua file
-	local mylua, err = loadfile('.' .. songdir .. path)
-	if err then
-		-- try additional song folders
-		local additionalsongfolders = PREFSMAN:GetPreference('AdditionalSongFolders')
-		-- cut off 'Songs/' from the path
-		local _,index = string.find(songdir,'Songs/')
-		local songloc = string.sub(songdir,index)
-		
-		-- for every songfolder in the additionalsongfolders
-		for songfolder in string.gfind(additionalsongfolders,'[^,]+') do
-			local err
-			
-			-- attempt to load file
-			mylua, err = loadfile(songfolder .. songloc .. path)
-			if not err then break end
-		end
-	end
-	-- report error if nothing was found
-	if not mylua then
-		SCREENMAN:SystemMessage(err)
-		return
-	else
-		local success, result = pcall(mylua)
-		if success then
-			return result
-		else
-			SCREENMAN:SystemMessage(result)
-			return
-		end
-	end
-end
-
 return Def.ActorFrame {
 	InitCommand = function(self)
 		xero.foreground = self
-		xero.loadscript('template/sharedvars.lua')
+		assert(loadfile('template/sharedvars.lua'))()
 		self:sleep(9e9)
 	end,
-	xero.loadscript('outfox/std.lua'),
-	xero.loadscript('outfox/template.lua'),
-	xero.loadscript('outfox/ease.lua'),
-	xero.loadscript('outfox/plugins.lua'),
-	xero.loadscript('outfox/modport.lua'),
-	xero.loadscript('lua/mods.lua'),
+	assert(loadfile('outfox/std.lua'))(),
+	assert(loadfile('outfox/template.lua'))(),
+	assert(loadfile('outfox/ease.lua'))(),
+	assert(loadfile('outfox/plugins.lua'))(),
+	assert(loadfile('outfox/modport.lua'))(),
+	assert(loadfile('lua/mods.lua'))(),
 }
