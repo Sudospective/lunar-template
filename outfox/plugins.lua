77a05d3ab8432b6f1b4dc2eba9c87cf04288e4ed
Sudospective <avery@sudospective.net>
2021-05-29 13:42:07 -0500

    Added songdir to default.xml

--- outfox/plugins.lua
+++
@@ -1,8 +0,0 @@
-local path_to_plugins = GAMESTATE:GetCurrentSong():GetSongDir()..'plugins/'
-local af = Def.ActorFrame {}
-for _, filename in ipairs(FILEMAN:GetDirListing(path_to_plugins)) do
-	if string.sub(filename, -4, -1) == '.lua' then
-		af[#af + 1] = assert(loadfile(path_to_plugins..filename))()
-	end
-end
-return af
